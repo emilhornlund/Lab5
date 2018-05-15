4e44463e28a12000fa7794d7593a7de360379c14
Emil Hörnlund <emil.hornlund@me.com>
2018-05-15 20:00:52 +0200

    Test case for user story 5 Strike

--- src/Tests.java
+++ src/Tests.java
@@ -58,4 +58,22 @@ public class Tests {
 
         assertEquals(81, game.getTotalScore());
     }
+
+    @Test
+    public void strikeTest() {
+        Game game = new Game();
+
+        game.addFrame(10, 0);
+        game.addFrame(3, 6);
+        game.addFrame(7, 2);
+        game.addFrame(3, 6);
+        game.addFrame(4, 4);
+        game.addFrame(5, 3);
+        game.addFrame(3, 3);
+        game.addFrame(4, 5);
+        game.addFrame(8, 1);
+        game.addFrame(2, 6);
+
+        assertEquals(95, game.getTotalScore());
+    }
 }
