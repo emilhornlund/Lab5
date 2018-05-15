4b09ce1dfa645340c637b6847b28fc4d92c37b9f
Emil Hörnlund <emil.hornlund@me.com>
2018-05-15 19:26:57 +0200

    Test case for user story 2 Frame Score

--- src/Tests.java
+++ src/Tests.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class Tests {
@@ -7,5 +8,16 @@ public class Tests {
         Game game = new Game();
         assertTrue(game.addFrame(2, 4));
         assertFalse(game.addFrame(10, 10));
+    }
+
+    @Test
+    public void sumOfFrameScoreTest() {
+        Game game = new Game();
+
+        game.addFrame(2, 6);
+        assertEquals(2+6, game.getFrameAt(0).getScore());
+
+        game.addFrame(0, 9);
+        assertEquals(2+6, game.getFrameAt(0).getScore());
     }
 }
