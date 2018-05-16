71013f42af8ff5b6971df9a44b726adf76be8610
Emil Hörnlund <emil.hornlund@me.com>
2018-05-16 08:56:47 +0200

    Test case for 7 Strike and Spare

--- src/Tests.java
+++ src/Tests.java
@@ -94,4 +94,22 @@ public class Tests {
 
         assertEquals(88, game.getTotalScore());
     }
+
+    @Test
+    public void strikeAndSpareTest() {
+        Game game = new Game();
+
+        game.addFrame(10,0);
+        game.addFrame(4,6);
+        game.addFrame(7,2);
+        game.addFrame(3,6);
+        game.addFrame(4,4);
+        game.addFrame(5,3);
+        game.addFrame(3,3);
+        game.addFrame(4,5);
+        game.addFrame(8,1);
+        game.addFrame(2,6);
+
+        assertEquals(103, game.getTotalScore());
+    }
 }
